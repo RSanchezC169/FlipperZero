e7b7f5815119950e2b1f343fbe35fe312d594fed
RSanchezC169 <35344339+RSanchezC169@users.noreply.github.com>
2025-03-05 13:35:12 -0700

    Create Enable_Windows_Security.ps1

---
+++ BadUsb/Windows/Malicious/Enable_Windows_Security.ps1
@@ -0,0 +1,81 @@
+##################################################################################################################################################################
+##################################################################################################################################################################
+<#
+                 _..--+~/@-~--.
+             _-=~      (  .   "}
+          _-~     _.--=.\ \""""
+        _~      _-       \ \_\
+       =      _=          '--'
+      '      =                             .
+     :      :       ____                   '=_. ___
+___  |      ;                            ____ '~--.~.
+     ;      ;                               _____  } |
+  ___=       \ ___ __     __..-...__           ___/__/__
+     :        =_     _.-~~          ~~--.__
+_____ \         ~-+-~                   ___~=_______
+     ~@#~~ == ...______ __ ___ _--~~--_
+                                                    =
+██████╗ ███████╗ █████╗ ███╗   ██╗ ██████╗██╗  ██╗███████╗███████╗ ██████╗ ██╗ ██████╗ █████╗ 
+██╔══██╗██╔════╝██╔══██╗████╗  ██║██╔════╝██║  ██║██╔════╝╚══███╔╝██╔════╝███║██╔════╝██╔══██╗
+██████╔╝███████╗███████║██╔██╗ ██║██║     ███████║█████╗    ███╔╝ ██║     ╚██║███████╗╚██████║
+██╔══██╗╚════██║██╔══██║██║╚██╗██║██║     ██╔══██║██╔══╝   ███╔╝  ██║      ██║██╔═══██╗╚═══██║
+██║  ██║███████║██║  ██║██║ ╚████║╚██████╗██║  ██║███████╗███████╗╚██████╗ ██║╚██████╔╝█████╔╝
+╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚══════╝ ╚═════╝ ╚═╝ ╚═════╝ ╚════╝ 
+Script Version: 1
+OS Version Script was written on: Microsoft Windows 11 Pro : 10.0.25100 Build 26100
+PSVersion 5.1.26100.2161 : PSEdition Desktop : Build Version 10.0.26100.2161
+Flipper Zero FirmWare mntm-001 https://momentum-fw.dev/update/
+Description of Script: 
+#>
+##################################################################################################################################################################
+#==============================Beginning==========================================================================================================================
+##################################################################################################################################################################
+function Enable-AllSecurityFeatures {
+    # Enable Windows Defender Real-Time Protection
+    Set-MpPreference -DisableRealtimeMonitoring $false
+
+    # Start Windows Defender services
+    Set-Service -Name "WinDefend" -StartupType Automatic
+    Start-Service -Name "WinDefend"
+
+    # Enable Windows Firewall for all profiles
+    Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
+
+    # Enable ASR Rules
+    Set-MpPreference -AttackSurfaceReductionRules_Actions Enable
+
+    # Enable Controlled Folder Access
+    Set-MpPreference -EnableControlledFolderAccess Enabled
+
+    # Enable Exploit Protection
+    Set-ProcessMitigation -PolicyName "Default" -Option On
+
+    # Enable UAC
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -Value 1
+
+    # Enable Windows SmartScreen
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" -Name "SmartScreenEnabled" -Value "Prompt"
+
+    # Enable Network Inspection System
+    Set-MpPreference -DisableIntrusionPreventionSystem $false
+
+    # Enable Tamper Protection (Requires reboot)
+    Set-MpPreference -DisableTamperProtection $false
+
+    # Enable Windows Defender Application Guard
+    Enable-WindowsOptionalFeature -Online -FeatureName Windows-Defender-ApplicationGuard
+
+    # Enable Windows Defender Credential Guard
+    reg add "HKLM\SYSTEM\CurrentControlSet\Control\LSA" /v LsaCfgFlags /t REG_DWORD /d 1 /f
+
+    # Enable Scheduled Scans
+    Set-MpPreference -DisableScheduleTaskCheck $false
+
+    Write-Output "All specified security features have been enabled."
+}
+
+# Call the function to enable all security features
+Enable-AllSecurityFeatures
+##################################################################################################################################################################
+#==============================End================================================================================================================================
+##################################################################################################################################################################
